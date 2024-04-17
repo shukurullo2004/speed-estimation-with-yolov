8969beede5c84ef1cb737dffef9b282954665c3d
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2024-04-17 22:07:37 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from ultralytics import YOLO
+from ultralytics.solutions import speed_estimation
+import cv2
+import sys
+print("Python interpreter path:", sys.executable)
+
+model = YOLO("yolov8n.pt")
+names = model.model.names
+
+cap = cv2.VideoCapture('roadt.mp4')
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+# Video writer
+video_writer = cv2.VideoWriter("speed_estimation.avi",
+                               cv2.VideoWriter_fourcc(*'mp4v'),
+                               fps,
+                               (w, h))
+
+line_pts = [(0, 720), (w, 720)]
+
+# Init speed-estimation obj
+speed_obj = speed_estimation.SpeedEstimator()
+speed_obj.set_args(reg_pts=line_pts,
+                   names=names,
+                   view_img=True)
+
+while cap.isOpened():
+
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+
+    tracks = model.track(im0, save = True, persist=True, show=False)
+
+    im0 = speed_obj.estimate_speed(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
